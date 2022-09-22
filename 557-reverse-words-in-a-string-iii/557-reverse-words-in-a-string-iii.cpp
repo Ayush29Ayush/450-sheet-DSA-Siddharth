d7bdd85445b918c33acb05e702d69f16b19f68d0
Ayush Senapati <ayushsenapati123@gmail.com>
2022-09-22 22:40:36 +0530

    Reverse Words in a String III - LC557

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,15 +1,49 @@
 // https://leetcode.com/problems/reverse-words-in-a-string-iii/discuss/2608114/C%2B%2B-or-Stringstream-or-Easy-Approach
-class Solution{
+// class Solution{
+// public:
+//     string reverseWords(string s){
+//         stringstream ss(s);
+//         string word;
+//         string ans = "";
+//         while (ss >> word){ // this will read the string word by word
+//             reverse(word.begin(), word.end());
+//             ans += word + " ";
+//         }
+//         ans.pop_back(); // this line is basically to remove the last space
+//         return ans;
+//     }
+// };
+
+class Solution {
 public:
-    string reverseWords(string s){
-        stringstream ss(s);
-        string word;
+    string reverseWords(string s) {
+        stack<char> st;
         string ans = "";
-        while (ss >> word){ // this will read the string word by word
-            reverse(word.begin(), word.end());
-            ans += word + " ";
+        
+        for(int i = 0; i<s.length(); i++)
+        {
+            if(s[i]==' ')
+            {
+                while(!st.empty())
+                {
+                    ans += st.top();
+                    st.pop();
+                }
+                ans += " ";
+            }
+            else
+            {
+                st.push(s[i]);
+            }
         }
-        ans.pop_back(); // this line is basically to remove the last space
+        
+        while(!st.empty())
+        {
+            ans += st.top();
+            st.pop();
+        }
+        
+        
         return ans;
     }
 };
