5041c0b309c1a73f47f0630d961910e5bb6234ea
Ayush Senapati <ayushsenapati123@gmail.com>
2022-11-18 12:37:49 +0530

    Median in a row-wise sorted Matrix

---
+++ GFG - Median in a row-wise sorted Matrix/median-in-a-rowwise-sorted-matrix.cpp
@@ -0,0 +1,47 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+    int median(vector<vector<int>> &matrix, int R, int C){
+        // code here   
+        vector<int> arr;
+        for(int i=0;i<R;i++){
+            for(int j=0;j<C;j++){
+                arr.push_back(matrix[i][j]);
+            }
+        }
+        sort(arr.begin(),arr.end());
+        int median = arr[(arr.size()-1)/2];
+        return median;
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int r, c;
+        cin>>r>>c;
+        vector<vector<int>> matrix(r, vector<int>(c));
+        for(int i = 0; i < r; ++i)
+            for(int j = 0;j < c; ++j)
+                cin>>matrix[i][j];
+        Solution obj;
+        cout<<obj.median(matrix, r, c)<<endl;        
+    }
+    return 0;
+}
+// } Driver Code Ends
