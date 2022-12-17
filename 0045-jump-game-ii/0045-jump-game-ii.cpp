2b124a2788f419c89d6b008d06e2bea15dff07aa
Ayush Senapati <ayushsenapati123@gmail.com>
2022-12-17 23:55:26 +0530

    Jump Game II - LC45

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,21 @@
+//! Greedy approach
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int jumps = 0;
+        int current = 0;
+        int farthest = 0;
+        
+        for(int i=0; i<nums.size()-1; i++)
+        {
+            farthest = max(farthest, nums[i]+i);
+            
+            if(i==current)
+            {
+                current = farthest;
+                jumps++;
+            }
+        }
+        return jumps;
+    }
+};
