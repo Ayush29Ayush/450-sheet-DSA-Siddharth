f7a7d717bbf00074ca429507fde3d2caeed733b2
Ayush Senapati <ayushsenapati123@gmail.com>
2022-12-18 22:48:31 +0530

    updated comments

--- 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.cpp
@@ -13,8 +13,9 @@ public:
         double time_sum = 0;
         for(int i=0; i<piles.size(); i++)
         {
+            // basically ceil() value leni hai
             // for ex-> speed = 5 and piles[i]=7 so, ek baar mein 5 and dusri baar mein 2. so 7 ko khaane mein 2hour laga
-            // so quotient + remainder
+            // so quotient + 1(if remainder!=0)
             time_sum += (piles[i]/mid)+((piles[i]%mid)!=0);
         }
         return time_sum;
