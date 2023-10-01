4f2303c6d5ccb72d91cb0cda92d2d115e3da1654
Ayush Senapati <ayushsenapati123@gmail.com>
2023-10-01 20:52:49 +0530

    Reverse Integer - LC007

--- 0007-reverse-integer/0007-reverse-integer.cpp
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -1,20 +1,38 @@
+// //! Logic correct but not working idk why?
+// class Solution {
+// public:
+//     int reverse(int x) {
+//         string number = to_string(x);
+//         int n = number.size();
+//         int ans = 0;
+        
+//         for(int i=n-1; i>=0; i--)
+//         {
+//             ans = ans + int(number[i])*pow(10,i);
+//         }
+        
+//         return ans;
+//     }
+// };
+
 class Solution {
 public:
     int reverse(int n) {
-        
         int ans = 0;
+        int rem = 0;
+        
         while(n!=0)
         {
-            int digit = n % 10;
+            rem = n%10;
             
-            if( (ans > INT_MAX/10) || (ans < INT_MIN/10) )
-            {
+            // ans*10 > INT_MAX karke check karenge toh int range se bahar chale jayega and error dega so same equation ko reframe karke check karlo
+            if(ans>INT_MAX/10 || ans<INT_MIN/10)
                 return 0;
-            }
             
-            ans = (ans*10) + digit;
-            n = n/10;   
+            ans = ans*10 + rem;
+            n = n/10;
         }
+        
         return ans;
     }
 };
