ccd940c983052749df87670f1bb341b9d95f4781
Ayush Senapati <61515726+Ayush29Ayush@users.noreply.github.com>
2024-09-21 10:59:32 +0530

    Combine Two Tables - LC175

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++
@@ -1,6 +0,0 @@
-# Write your MySQL query statement below
-# -- left join kiya because left side ka sab chahiye but right side ka agar nahi mile toh null kar dena
-
-SELECT firstName, lastName, city, state
-FROM Person p LEFT JOIN Address a
-ON p.personId = a.personId
